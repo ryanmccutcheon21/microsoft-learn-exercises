1e949d3db3f42b8a7423891c479359d2fb1bcefe
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-20 17:16:16 -0500

    Guided Project - Calculate and Print Student Grades

--- first.cs
+++ first.cs
@@ -68,3 +68,41 @@ Console.WriteLine(12.39816m);
 // Add the following lines of code in the code editor:
 Console.WriteLine(true);
 Console.WriteLine(false);
+
+
+
+
+
+// Guided Project - Calculate and Print Student Grades
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+Console.WriteLine($@"Student    Grade
+Sophia     {((sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / (decimal)currentAssignments)} A
+Nicolas    {((nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / (decimal)currentAssignments)} B
+Zahirah    {((zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / (decimal)currentAssignments)} B
+Jeong      {((jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / (decimal)currentAssignments)} A");
