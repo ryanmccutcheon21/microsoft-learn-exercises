82b15afaf5e4d241fc2a21b3c842a9bfa186782e
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-20 07:20:28 -0500

    First and second exercises

---
+++ first.cs
@@ -0,0 +1,9 @@
+// First exercise
+// write 'Hello World' to console
+Console.WriteLine("Hello World");
+
+// Second exercise
+// Comment out first exercise, then write Congratulations! You wrote your first lines of code.
+Console.Write("Congratulations!")
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
