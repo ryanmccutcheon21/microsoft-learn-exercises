10b3c676d95d6da5b3a617321d6bbf522a3e2407
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-20 17:32:39 -0500

    Guided Project - Calculate final GPA Step 1

--- first.cs
+++ first.cs
@@ -238,3 +238,24 @@ Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
 Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
 Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
 Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+
+
+
+
+
+// Guided Project
+
+// Calculate Final GPA
+// Setup
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
