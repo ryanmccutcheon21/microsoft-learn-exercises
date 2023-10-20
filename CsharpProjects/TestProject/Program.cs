334221f5f3fbea5227fbf75350b3f60bdff064f5
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-20 18:50:19 -0500

    Math class methods challenge

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -6,3 +6,12 @@ int roll3 = dice.Next(50, 101);
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
+
+
+// Exercise - Complete a challenge activity to discover and implement a method call
+// Math class methods challenge
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
