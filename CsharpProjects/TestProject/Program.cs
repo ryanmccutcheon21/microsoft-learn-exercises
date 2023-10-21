48e59038e5ac141c4c3d2c0963bdaeab5345422c
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-20 19:00:18 -0500

    create nested decision logic with if, else if, and else statements

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -24,6 +24,44 @@
 
 // Exercise - Create decision logic with if statements
 
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// if ((roll1 == roll2) && (roll2 == roll3))
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+
+
+
+
+// Exercise - Create nested decision logic with if, else if, and else
+
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
@@ -36,22 +74,31 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
