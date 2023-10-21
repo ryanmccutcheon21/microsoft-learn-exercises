450b5a370187daa993ec516678a63ea1f35b5e87
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-21 16:15:33 -0500

    improving code readability with comments and whitespace

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -206,3 +206,35 @@ foreach (string order in investigateOrders)
         Console.WriteLine($"Investigate order {order}");
     }
 }
+
+
+
+
+
+// Exercise - Complete a challenge activity to improve code readability
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
