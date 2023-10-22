b9d6efd27f247eb33f92a76a7f8f642c1a0ba45d
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-22 12:53:15 -0500

    Challenge Activity 2 - Evaluate boolean expressions

---
+++ Part3/ChallengeActivity2/Program.cs
@@ -0,0 +1,24 @@
+﻿// string permission = "Admin|Manager";
+string permission = "Manager";
+int level = 19;
+
+if (permission.Contains("Admin") && level > 55)
+{
+    Console.WriteLine("Welcome, Super Admin user.");
+}
+else if (permission.Contains("Admin") && level <= 55)
+{
+    Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else if (permission.Contains("Manager") && level < 20)
+{
+    Console.WriteLine("You do not have sufficient privileges to access this");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
