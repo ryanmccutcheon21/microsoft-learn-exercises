531abcc7118836123ab8b28698ed72c6d72fb03b
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-22 12:41:31 -0500

    Evaluate an expression

---
+++ Part3/TestProject/Program.cs
@@ -0,0 +1,47 @@
+﻿// This module contains C# code to learn how to evaluate expressions in Microsoft's Foundational C# course
+
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+
+// Using the inequality operator
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue2 = "a";
+Console.WriteLine(myValue2 != "a");
+
+
+// Using comaprison operators
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine("Using comparison operators");
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+
+
+// Methods that return a boolean value
+Console.WriteLine("\n");
+Console.WriteLine("\n");
+Console.WriteLine("Methods that return a boolean value");
+
+string pangram = "The quick brown fox jumped over the lazy dog";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+// Logical negation
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
