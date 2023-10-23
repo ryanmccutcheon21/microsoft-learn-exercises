24fa6045be42edd26898986e600a3ce5c35008e0
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-22 19:23:46 -0500

    iterate using for loop

---
+++ For/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+// Use the break keyword to break the iteration statement
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+
+
+// Loop through each element of an array
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
