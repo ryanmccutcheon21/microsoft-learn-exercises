d7eb47fad1b1eb22d8d843823fd72c8bcb2bede7
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-10-23 21:11:16 -0500

    challenge 2 setup

--- Part5/Program.cs
+++ Part5/Program.cs
@@ -1,83 +1,173 @@
-﻿// Console.WriteLine("Generating random numbers:");
-// DisplayRandomNumbers();
+﻿// // Console.WriteLine("Generating random numbers:");
+// // DisplayRandomNumbers();
 
-// void DisplayRandomNumbers()
+// // void DisplayRandomNumbers()
+// // {
+// //     Random random = new Random();
+
+// //     for (int i = 0; i < 5; i++)
+// //     {
+// //         Console.Write($"{random.Next(1, 100)} ");
+// //     }
+
+// //     Console.WriteLine();
+// // }
+
+
+// // create reusuable methods
+// using System;
+
+// int[] times = { 800, 1200, 1600, 2000 };
+// int diff = 0;
+
+// Console.WriteLine("Enter current GMT");
+// int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Current Medicine Schedule:");
+// DisplayTimes();
+
+// Console.WriteLine("Enter new GMT");
+// int newGMT = Convert.ToInt32(Console.ReadLine());
+
+// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 // {
-//     Random random = new Random();
+//     Console.WriteLine("Invalid GMT");
+// }
+// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+// {
+//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     AdjustTimes();
+// }
+// else
+// {
+//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     AdjustTimes();
+// }
 
-//     for (int i = 0; i < 5; i++)
+// Console.WriteLine("New Medicine Schedule:");
+// DisplayTimes();
+
+// void DisplayTimes()
+// {
+//     /* Format and display medicine times */
+//     foreach (int val in times)
 //     {
-//         Console.Write($"{random.Next(1, 100)} ");
-//     }
+//         string time = val.ToString();
+//         int len = time.Length;
 
+//         if (len >= 3)
+//         {
+//             time = time.Insert(len - 2, ":");
+//         }
+//         else if (len == 2)
+//         {
+//             time = time.Insert(0, "0:");
+//         }
+//         else
+//         {
+//             time = time.Insert(0, "0:0");
+//         }
+
+//         Console.Write($"{time} ");
+//     }
 //     Console.WriteLine();
 // }
 
+// void AdjustTimes()
+// {
+//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         times[i] = ((times[i] + diff)) % 2400;
+//     }
+// }
 
-// create reusuable methods
-using System;
 
-int[] times = { 800, 1200, 1600, 2000 };
-int diff = 0;
 
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Current Medicine Schedule:");
-DisplayTimes();
+// // create methods with parameters
+// string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
 
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
+// DisplayStudents(students);
+// DisplayStudents(new string[] { "Robert", "Vanya" });
 
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// void DisplayStudents(string[] students)
+// {
+//     foreach (string student in students)
+//     {
+//         Console.Write($"{student}, ");
+//     }
+//     Console.WriteLine();
+// }
+
+// double pi = 3.14159;
+// PrintCircleInfo(12);
+// PrintCircleInfo(24);
+
+// void PrintCircleInfo(int radius)
+// {
+//     Console.WriteLine($"Circle with radius {radius}");
+//     PrintCircleArea(radius);
+//     PrintCircleCircumference(radius);
+// }
+
+// void PrintCircleArea(int radius)
+// {
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+// void PrintCircleCircumference(int radius)
+// {
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+
+
+
+
+// methods with optional parameters
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
 {
-    Console.WriteLine("Invalid GMT");
-}
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    AdjustTimes();
-}
-else
-{
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    AdjustTimes();
-}
-
-Console.WriteLine("New Medicine Schedule:");
-DisplayTimes();
-
-void DisplayTimes()
-{
-    /* Format and display medicine times */
-    foreach (int val in times)
+    if (inviteOnly)
     {
-        string time = val.ToString();
-        int len = time.Length;
-
-        if (len >= 3)
+        bool found = false;
+        foreach (string guest in guestList)
         {
-            time = time.Insert(len - 2, ":");
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
         }
-        else if (len == 2)
+        if (!found)
         {
-            time = time.Insert(0, "0:");
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
         }
-        else
-        {
-            time = time.Insert(0, "0:0");
-        }
-
-        Console.Write($"{time} ");
     }
-    Console.WriteLine();
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
 }
 
-void AdjustTimes()
+void ShowRSVPs()
 {
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++)
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
     {
-        times[i] = ((times[i] + diff)) % 2400;
+        Console.WriteLine(rsvps[i]);
     }
 }
